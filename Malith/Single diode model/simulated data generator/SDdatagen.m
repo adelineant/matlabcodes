524f56ddbbe548b2e74648d07977d7a92921900c
adelineant <37828338+adelineant@users.noreply.github.com>
2018-04-19 13:23:00 +1000

    Modified the program to make several simulated data files using for loops. Need to add noise near the V = 0 to make it more real

--- Malith/Single diode model/simulated data generator/SDdatagen.m
+++ Malith/Single diode model/simulated data generator/SDdatagen.m
@@ -1,23 +1,38 @@
 clc,clear all, close all
 
-V = 0:0.01:0.7;
-Rs = 0.0364;
-n = 1.484;
-Rsh = 0.0538*1000;
-Iph =0.7608;
-I0 =0.3223*10^-6;
-T = 306.15;
+for Rs = linspace(1e-8,1e-3,5)
+    for Rsh = linspace(8,1,5)
+        for n = linspace(1,3,6)
+            
+            V = 0:0.01:1;
+            Iph =0.7608;
+            I0 =0.3223*10^-6;
+            T = 306.15;
 
-%y=SD_equation(V,I,Rs,Rsh,n)
-Ical = zeros(size(V));
+            %y=SD_equation(V,I,Rs,Rsh,n)
+            Ical = zeros(size(V));
 
-Ical(1)= fzero(@(I)SD_equation(V(1),I,Rs,Rsh,n,Iph,I0,T),-0.3);
+            Ical(1)= fzero(@(I)SD_equation(V(1),I,Rs,Rsh,n,Iph,I0,T),-0.3);
 
-for i=1:length(V)-1
-    Ical(i+1) = fzero(@(I)SD_equation(V(i+1),I,Rs,Rsh,n,Iph,I0,T),1);
+            for i=1:length(V)-1
+                Ical(i+1) = fzero(@(I)SD_equation(V(i+1),I,Rs,Rsh,n,Iph,I0,T),1);
+            end
+            
+            %plot (V,-Ical)
+           % hold on
+            
+            x = 0:.1:1;
+            A = [V;-Ical];
+            format = sprintf("Rs=%e_Rsh=%e_n=%f.txt",Rs,Rsh,n);
+            
+
+            fileID = fopen(format,'w');
+            fprintf(fileID,'%6s %12s\r\n','V','Ical');
+            fprintf(fileID,'%6.2f %12.8f\r\n',A);
+            fclose(fileID);
+        end
+    end
 end
-plot (V,-Ical)
-
 
 
 function y=SD_equation(V,I,Rs,Rsh,n,Iph,I0,T)
